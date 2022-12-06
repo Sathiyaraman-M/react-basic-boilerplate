192492820a514a8152a6451be8a421becd36f625
SathiyaramanM <sathiyaraman2003@gmail.com>
2022-12-06 21:16:02 +0530

    Updated FetchData page

--- src/Pages/FetchData.js
+++ src/Pages/FetchData.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { Table } from "reactstrap";
 
 const FetchData = () => {
+    const object = [
+        {
+            Weather: 'Sunny',
+            TempC: 32
+        },
+        {
+            Weather: 'Cold',
+            TempC: 14
+        },
+        {
+            Weather: 'Freezing',
+            TempC: 7
+        },
+        {
+            Weather: 'Rainy',
+            TempC: 26
+        },
+        {
+            Weather: 'Moody',
+            TempC: 29
+        }
+    ]
     return (
         <>
-            <span>Fetch Data page</span>
+            <h1>Fetch Data</h1>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Weather</th>
+                        <th>TempC</th>
+                        <th>TempF</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {object.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.Weather}</td>
+                            <td>{val.TempC} C</td>
+                            <td>{val.TempC * 9 / 5 + 32} F</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </Table>
         </>
     )
 };
